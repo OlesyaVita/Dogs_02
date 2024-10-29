3c5eeef7ca80b10d8b317510f47015f5d205a46b
KustovyKOAL <lesyavita@ya.ru>
2024-10-30 00:15:24 +0300

    Создана функция загрузки ссылки на изображение

--- Dogs_02.py
+++ Dogs_02.py
@@ -1,32 +1,45 @@
-from tkinter import *
 import requests
+from tkinter import *
 from PIL import Image, ImageTk
 from io import BytesIO
 from tkinter import messagebox as mb
 
+
+def get_random_dog_image():
+    try:
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        response.raise_for_status()
+        data = response.json()
+        return data['message']
+    except Exception as e:
+        mb.showerror("Ошибка", f"Ошибка при запросе к API: {e}")
+        return None
+
+
 def show_image():
-    image_url = get_dog_image()
+    image_url = get_random_dog_image()
     if image_url:
         try:
-            respone = requests.get(image_url, stream=true)
-            respone.raise_for_status()
-            img_data = BytesIO(respone.content)
-            img = Image.open(img_date)
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
             img.thumbnail((300, 300))
-            lable.config(image=img)
-            lable.image = img
-        except Exception as e:
-            mb.showerror("Ошибка", message=f"Возникла ошибка {e}")
+            img = ImageTk.PhotoImage(img)
+            label.config(image=img)
+            label.image = img
+
+        except requests.RequestException as e:
+            mb.showerror("Ошибка", f"Не удалось загрузить изображение: {e}")
 
 
 window = Tk()
 window.title("Картинки с собачками")
 window.geometry("360x420")
 
-lable = Label()
-lable.pack(pady=10)
+label = Label()
+label.pack(padx=10, pady=10)
 
 button = Button(text="Загрузить изображение", command=show_image)
-button.pack(pady=10)
+button.pack(padx=10, pady=10)
 window.mainloop()
-
