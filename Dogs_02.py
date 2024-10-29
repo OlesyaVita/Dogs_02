bb352141e8f9f616f217354bb224da2f8b69a3db
KustovyKOAL <lesyavita@ya.ru>
2024-10-30 01:31:56 +0300

    Каждое изображение в отдельном окне. Добавлены спинбоксы для установки размера изображения

--- Dogs_02.py
+++ Dogs_02.py
@@ -1,9 +1,10 @@
 import requests
-from tkinter import *
+from tkinter import Tk, Toplevel, messagebox
+from tkinter import ttk
 from PIL import Image, ImageTk
 from io import BytesIO
 from tkinter import messagebox as mb
-from tkinter import ttk
+
 
 def get_random_dog_image():
     try:
@@ -11,45 +12,57 @@ def get_random_dog_image():
         response.raise_for_status()
         data = response.json()
         return data['message']
-    except Exception as e:
+    except requests.RequestException as e:
         mb.showerror("Ошибка", f"Ошибка при запросе к API: {e}")
         return None
 
-
-
 def show_image():
     image_url = get_random_dog_image()
+
     if image_url:
         try:
             response = requests.get(image_url, stream=True)
             response.raise_for_status()
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
-            img.thumbnail((300, 300))
+            img_size = (int(width_spinbox.get()), int(height_spinbox.get()))
+            img.thumbnail(img_size)
             img = ImageTk.PhotoImage(img)
-            label.config(image=img)
+
+            new_window = Toplevel(window)
+            new_window.title("Случайное изображение пёсика")
+            label = ttk.Label(new_window, image=img)
             label.image = img
+            label.pack(padx=10, pady=10)
 
         except requests.RequestException as e:
             mb.showerror("Ошибка", f"Не удалось загрузить изображение: {e}")
-    progress.stop()
 
-def prog():
-    progress["value"] = 0
+def start_progress():
+    progress['value'] = 0
     progress.start(30)
-    window.after(3000, show_image)
+    window.after(3000, lambda: [progress.stop(), show_image()])
 
 window = Tk()
-window.title("Картинки с собачками")
-window.geometry("360x420")
+window.title("Случайное изображение")
 
-label = ttk.Label()
-label.pack(padx=10, pady=10)
-
-button = ttk.Button(text="Загрузить изображение", command=prog)
+button = ttk.Button(window, text="Загрузить изображение", command=start_progress)
 button.pack(padx=10, pady=10)
 
-progress = ttk.Progressbar(mode="determinate", length=300)
-progress.pack(pady=10)
+progress = ttk.Progressbar(window, mode='determinate', length=300)
+progress.pack(padx=10, pady=5)
+
+
+# Ширина
+width_label = ttk.Label(text="Ширина:")
+width_label.pack(side='left', padx=(10, 0))
+width_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+width_spinbox.pack(side='left', padx=(0, 10))
+
+# Высота
+height_label = ttk.Label(text="Высота:")
+height_label.pack(side='left', padx=(10, 0))
+height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+height_spinbox.pack(side='left', padx=(0, 10))
 
 window.mainloop()
